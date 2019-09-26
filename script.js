b693130545bbba526d115d206b12485092c94552
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-09-26 21:14:18 +0200

    handle out of country periods

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getTimeRemaining = (endtime) => {
-  const t = Date.parse(endtime) - Date.parse(new Date());
+  const t = Date.parse(endtime) - Date.now();
   const seconds = Math.floor((t / 1000) % 60);
   const minutes = Math.floor((t / 1000 / 60) % 60);
   const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
@@ -13,7 +13,7 @@ const getTimeRemaining = (endtime) => {
   };
 }
 
-const initializeClock = (id, endtime) => {
+const initializeClock = (id, endtime, OOC) => {
   const clock = document.getElementById(id);
   const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
@@ -21,7 +21,25 @@ const initializeClock = (id, endtime) => {
   const secondsSpan = clock.querySelector('.seconds');
 
   const updateClock = () => {
-    const t = getTimeRemaining(endtime);
+
+    const now = Date.now()
+    let toAdd = 0
+
+    for (out of OOC) {
+      // Do we need to add any time?
+      if ( now > Date.parse(out.start)){
+        if ( now > Date.parse(out.end)){
+          // need to add the full period
+          toAdd += (Date.parse(out.end) - Date.parse(out.start))
+        } else {
+          // add only time spent since beginning of period to freeze time
+          toAdd += Date.now() - Date.parse(out.start)
+        }
+      }
+    }
+    
+    const correctedEnd = new Date(Date.parse(endtime) + toAdd)
+    const t = getTimeRemaining(correctedEnd);
 
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
@@ -38,6 +56,11 @@ const initializeClock = (id, endtime) => {
 }
 
 const dueDays = 730
-const daysOutside = 0
-const deadline = new Date(Date.parse(new Date("2019/09/16")) + (dueDays + daysOutside) * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+// out of country
+const OOC = [
+  {'start': '2019/10/17', 'end': '2019/10/31'}
+]
+
+const deadline = new Date(Date.parse(new Date('2019/09/16')) + dueDays * 24 * 60 * 60 * 1000);
+
+initializeClock('clockdiv', deadline, OOC);
